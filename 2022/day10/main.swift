886c155368435061fe333f628fb9babe623041a3
Timothy J. Wood <tjw@omnigroup.com>
2022-12-10 23:53:05 -0800

    2022 day 10 part 2

--- 2022/day10/main.swift
+++ 2022/day10/main.swift
@@ -7,9 +7,6 @@
 
 import Foundation
 
-let clockRate = 1 // ???
-var X = 1
-
 // addv 2 cycles to complete. After 2 cyles X is X += v (can be negative)
 // noop takes 1 cycle to complete
 // A cycle starts on the given instruction so
@@ -17,34 +14,89 @@ var X = 1
 // starts on the leading side and completes on the trailing side
 // instructions are synchronous, so an addv doesn't start until all the previous instructions have committed
 
-var cycle = 0
-var strength = 1
+let lines = Input.lines()
 
-var totalStrength = 0
+do {
+    var X = 1
+    var cycle = 0
+    var strength = 1
 
-func nextCycle() {
-    cycle += 1
+    var totalStrength = 0
 
-    if cycle == 20 || (cycle - 20) % 40 == 0 {
-        strength = cycle * X
-        totalStrength += strength
+    func nextCycle() {
+        cycle += 1
+
+        if cycle == 20 || (cycle - 20) % 40 == 0 {
+            strength = cycle * X
+            totalStrength += strength
+        }
+    }
+    lines.forEach { line in
+        let components = line.split(separator: " ")
+        if components[0] == "noop" {
+            nextCycle()
+        } else if components[0] == "addx" {
+            nextCycle()
+            nextCycle()
+            X += Int(components[1])!
+        } else {
+            fatalError()
+        }
     }
 
-    print("cycle \(cycle), X \(X)")
-}
-for line in Input.lines() {
-    let components = line.split(separator: " ")
-    if components[0] == "noop" {
-        nextCycle()
-    } else if components[0] == "addx" {
-        nextCycle()
-        nextCycle()
-        X += Int(components[1])!
-    } else {
-        fatalError()
-    }
+    print("\(totalStrength)")
+    assert(totalStrength == 15880)
 }
 
-print("\(totalStrength)")
-assert(totalStrength == 15880)
-// 37 not right
+do {
+    var X = 1
+    var cycle = 0
+
+    var previousScanlines = [String]()
+    var currentScanline = ""
+
+    func nextCycle() {
+        // Draw pixel
+        let pixelX = cycle % 40
+        if abs(pixelX - X) <= 1 {
+            currentScanline.append("#") // Lit
+        } else {
+            currentScanline.append(".") // Dark
+        }
+
+        cycle += 1
+        if cycle % 40 == 0 {
+            previousScanlines.append(currentScanline)
+            currentScanline = ""
+        }
+    }
+
+    lines.forEach { line in
+        let components = line.split(separator: " ")
+        if components[0] == "noop" {
+            nextCycle()
+        } else if components[0] == "addx" {
+            nextCycle()
+            nextCycle()
+            X += Int(components[1])!
+        } else {
+            fatalError()
+        }
+    }
+
+    if !currentScanline.isEmpty {
+        previousScanlines.append(currentScanline)
+    }
+
+    previousScanlines.forEach { print($0) }
+
+//    let expected = """
+//###..#.....##..####.#..#..##..####..##..
+//#..#.#....#..#.#....#.#..#..#....#.#..#.
+//#..#.#....#....###..##...#..#...#..#....
+//###..#....#.##.#....#.#..####..#...#.##.
+//#....#....#..#.#....#.#..#..#.#....#..#.
+//#....####..###.#....#..#.#..#.####..###.
+//"""
+//    assert(previousScanlines.joined() + "\n" == expected)
+}
