bd6a90e41991ae49a8603bce7c5952587b557d51
Timothy J. Wood <tjw@omnigroup.com>
2022-12-13 19:19:42 -0800

    2022 day 13 part 1

--- 2022/day13/main.swift
+++ 2022/day13/main.swift
@@ -7,43 +7,77 @@
 
 import Foundation
 
-do {
-    func correctlyOrdered(left left_: ArraySlice<Any>, right right_: ArraySlice<Any>) -> Bool {
-        print("####")
-        var left = left_
-        var right = right_
+var testInputs: [Any] = [
+    [1,1,3,1,1],
+    [1,1,5,1,1],
 
-        while !left.isEmpty && !right.isEmpty {
-            let leftNext = left.first!; left = left.dropFirst()
-            let rightNext = right.first!; right = right.dropFirst()
+    [[1],[2,3,4]],
+    [[1],4],
 
-            print("leftNext \(leftNext)")
-            print("rightNext \(rightNext)")
+    [9],
+    [[8,7,6]],
 
-            if let leftInt = leftNext as? Int, let rightInt = rightNext as? Int {
-                if leftInt < rightInt {
-                    return true
-                }
+    [[4,4],4,4],
+    [[4,4],4,4,4],
+
+    [7,7,7,7],
+    [7,7,7],
+
+    [],
+    [3],
+
+    [[[]]],
+    [[]],
+
+    [1,[2,[3,[4,[5,6,7]]]],8,9],
+    [1,[2,[3,[4,[5,6,0]]]],8,9],
+]
+
+func correctlyOrdered(left left_: ArraySlice<Any>, right right_: ArraySlice<Any>) -> Bool? {
+    var left = left_
+    var right = right_
+
+    while !left.isEmpty && !right.isEmpty {
+        let leftNext = left.first!; left = left.dropFirst()
+        let rightNext = right.first!; right = right.dropFirst()
+
+        print("leftNext \(leftNext)")
+        print("rightNext \(rightNext)")
+
+        if let leftInt = leftNext as? Int, let rightInt = rightNext as? Int {
+            if leftInt < rightInt {
+                return true
+            } else if leftInt > rightInt {
+                return false
             }
-
-            if leftNext is Array<Any> || rightNext is Array<Any> {
-                let leftSlice = (leftNext as? [Any] ?? [leftNext as! Int])[...]
-                let rightSlice = (rightNext as? [Any] ?? [rightNext as! Int])[...]
-                return correctlyOrdered(left: leftSlice, right: rightSlice)
+        } else if leftNext is Array<Any> || rightNext is Array<Any> {
+            let leftSlice = (leftNext as? [Any] ?? [leftNext as! Int])[...]
+            let rightSlice = (rightNext as? [Any] ?? [rightNext as! Int])[...]
+            if let result =  correctlyOrdered(left: leftSlice, right: rightSlice) {
+                return result
             }
+        } else {
+            fatalError()
         }
+    }
 
-        if left.isEmpty {
-            return true
-        }
-
+    if left.isEmpty && right.isEmpty {
+        return nil
+    }
+    if left.isEmpty {
+        return true
+    } else if right.isEmpty {
         return false
     }
 
+    return true
+}
 
-    var outOfOrder = 0
+do {
+    var inOrderSum = 0
+    var pairIndex = 1
     var remaining = input[0..<input.count]
-    print("\(type(of: remaining))")
+
     while !remaining.isEmpty {
         // Top level is always an array
         let left = remaining.first! as! [Any]
@@ -52,12 +86,25 @@ do {
         let right = remaining.first! as! [Any]
         remaining = remaining.dropFirst()
 
-        if !correctlyOrdered(left: left[...], right: right[...]) {
-            outOfOrder += 1
+        print("####")
+        print("left \(left)")
+        print("right \(right)")
+
+        if let result = correctlyOrdered(left: left[...], right: right[...]) {
+            if result {
+                print("correct \(pairIndex)")
+                inOrderSum += pairIndex
+            } else {
+                print("wrong")
+            }
+        } else {
+            print("unknown")
         }
+
+        pairIndex += 1
     }
 
-    print("\(outOfOrder)")
-    // 44 incorrect
+    print("\(inOrderSum)")
+    assert(inOrderSum == 5659)
 }
 
