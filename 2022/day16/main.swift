95563bcc4040c38e5551a586452d91fdbb72f52d
Timothy J. Wood <tjw@omnigroup.com>
2022-12-18 04:46:04 -0800

    WIP on 2022 day 16 part 1

---
+++ 2022/day16/main.swift
@@ -0,0 +1,102 @@
+//
+//  main.swift
+//  day16
+//
+//  Created by Timothy Wood on 12/17/22.
+//
+
+import Foundation
+
+class Node : CustomStringConvertible {
+
+    let name: String
+    let rate: Int
+    let neighbors: [String]
+
+    init(name: String, rate: Int, neighbors: [String]) {
+        self.name = name
+        self.rate = rate
+        self.neighbors = neighbors
+    }
+
+    var description: String {
+        return name
+    }
+
+}
+
+let nodes = Input.lines().map { line in
+    let regex = /Valve ([A-Z]+) has flow rate=([0-9]+); tunnel(s?) lead(s?) to valve(s?) ([A-Z, ]+)/
+    let match = try! regex.firstMatch(in: line)!
+    let name = match.1
+    let rate = Int(match.2)!
+    let neighbors = match.6.split(separator: ",").map { String($0.trimmingCharacters(in: .whitespaces)) }
+    return Node(name: String(name), rate: rate, neighbors: neighbors)
+}
+let nodeByName = Dictionary(uniqueKeysWithValues: nodes.map { node in
+    return (node.name, node)
+})
+
+print("nodes = \(nodes)")
+
+// emit dot description
+print("graph day16 {")
+for node in nodes {
+    // only emit edges once
+    for dest in node.neighbors {
+        if dest > node.name {
+            print("  \(node.name) -- \(dest)")
+        }
+    }
+}
+print("}")
+
+let currentNode = "AA"
+
+// Calculate all pairs shortest paths for the graph just counting the number of hops (minutes) to make the trip. Not doing the Floyd-Warshall/whatever algorithm.
+
+struct Path : Hashable {
+    let a, b: String
+
+    // Since our graph is not directed, a->b and b->a are the same length.
+    init(a: String, b: String) {
+        if a < b {
+            self.a = a
+            self.b = b
+        } else {
+            self.a = b
+            self.b = a
+        }
+    }
+}
+
+let bestPath: [Path:Int]
+do {
+    var current = [Path:Int]()
+
+    for source in nodes {
+        var length = 1
+        var nextNodes = source.neighbors
+
+        while !nextNodes.isEmpty {
+            var newNodes = [String]()
+
+            for dest in nextNodes {
+                let pair = Path(a: source.name, b: dest)
+                if current[pair] == nil {
+                    current[pair] = length
+                    print("pair \(pair) length \(length)")
+                    newNodes.append(contentsOf: nodeByName[dest]!.neighbors)
+                }
+            }
+
+            nextNodes = newNodes
+            length += 1
+        }
+    }
+}
+
+
+
+// Mark valves with zero flow as already open
+let initialOpenValves = nodes.filter { $0.rate == 0 }
