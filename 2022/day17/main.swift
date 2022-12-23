7f20dda6693450340204bb7a7612695afd75ac3e
Timothy J. Wood <tjw@omnigroup.com>
2022-12-22 22:19:46 -0800

    Updates and more commentary

--- 2022/day17/main.swift
+++ 2022/day17/main.swift
@@ -126,14 +126,24 @@ func print(map: HashMap<MapElement>, highestRock: Int, range range_: ClosedRange
     print("+-------+")
 }
 
-func process(map: HashMap<MapElement>, rocksLimit: Int) -> Int {
+// Returns the relative height from the previous max height in the map
+func process(map: HashMap<MapElement>, rocksLimit: Int) -> [Int] {
     // The floor is at y = 0
 
     // Allow for the map having been partially filled already
     let bounds = map.bounds
-    var highestRock = bounds.y + bounds.height
+    let startingHighestRow: Int
+    if bounds.height > 0 {
+        startingHighestRow = bounds.y + bounds.height - 1
+    } else {
+        startingHighestRow = 0
+    }
+    //print("startingHighestRow \(startingHighestRow)")
 
-    print(map: map, highestRock: highestRock, range: max(1, highestRock - 5)...(highestRock + 8))
+    var relativeHeights = [Int]()
+    var highestRock = startingHighestRow
+
+    //print(map: map, highestRock: highestRock, range: max(1, highestRock - 5)...(highestRock + 8))
 
     var rocksDropped = 0
     var inputOffset = 0
@@ -179,15 +189,17 @@ func process(map: HashMap<MapElement>, rocksLimit: Int) -> Int {
                 break
             }
         }
+
+        relativeHeights.append(highestRock - startingHighestRow)
     }
 
-    return highestRock
+    return relativeHeights
 }
 
 if false {
     let map = HashMap<MapElement>(defaultElement: .air)
 
-    let result = process(map: map, rocksLimit: 2022)
+    let result = process(map: map, rocksLimit: 2022).last!
 
     print("result \(result)")
     assert(result == 3177)
@@ -196,20 +208,48 @@ if false {
 if true {
     let map = HashMap<MapElement>(defaultElement: .air)
 
-    var previousHeight = 0
+    // Experimenting with trying to find a periodic cycle of height added by adding enough rocks to get back to the "start" modulo the input size and rock type count. The first iteration will be special since it lands on a flat floor instead of a craggy top. But experimenting beyond that, my input ends up increasing height with a cycle period of two.
 
-    for rep in 0..<100 {
-        let height = process(map: map, rocksLimit: input.count * rocks.count)
-        print("rep \(rep) added height \(height - previousHeight)")
-        previousHeight = height
+    // Each repetition is rock count * input length rocks dropped (5 * 10091) = 50455
+    // For the test input, the first cycle is 308, and then every cycle after that is 280.
+    // For the real input, the first cycle is 78982 and then a cycle of 78973 and 78980 after that.
+
+    let rocksPerCycle = input.count * rocks.count
+
+    // Print out a few "small" cycles looking for the full orbit
+    for rep in 0..<23 {
+        let height = process(map: map, rocksLimit: rocksPerCycle).last!
+        print("rep \(rep) bounds \(map.bounds.height), added height \(height)")
     }
 
-    // each rep is rock count * input length rocks dropped (5 * 10091) = 50455
-    // rep 0 is 78982
-    // rep 1 is 78973
-    // rep 2 is 78980
-    // following reps are rep 1 and 2 cycled infinitely
+    // Need to do 1_000_000_000_000 operations, but that will overflow. Break it up in to 1_000_000 * 1_000_000 for a simplified 'big int' approach that will be easily interpreted when printed out
+    var rocks2 = 1_000_000, rocks1 = 0
+    var height2 = 0, height1 = 0
+    while rocks2 > 0 || rocks1 >= rocksPerCycle {
+        //print("rocks \(rocks2) \(rocks1), height \(height2) \(height1)")
+
+        // TODO: Upscale the step size to be closer to 1e6 to require fewer inner iterations
+
+        if rocks1 < rocksPerCycle {
+            rocks2 -= 1
+            rocks1 += 1_000_000
+        }
+        rocks1 -= rocksPerCycle
+
+        height1 += 308
+        if height1 >= 1_000_000 {
+            height1 -= 1_000_000
+            height2 += 1
+        }
+    }
+
+    // TODO: Handle the last bit be indexing into the array of height increases for the full cycle.
+
+
+    // For test input, looking for 1_514_285 714_288
+
+    print("rocks \(rocks2) \(rocks1)")
+    print("height \(height2) \(height1)")
 }
 
 
-// 1_000_000_000_000
