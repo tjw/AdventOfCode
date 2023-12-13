a1928f63db6b55273618873486e20bd316857fc1
Timothy J. Wood <tjw@omnigroup.com>
2023-12-12 18:36:44 -0800

    Day 12 part 1 works, and part 2 is slow even with test input

--- 2023/day12/main.swift
+++ 2023/day12/main.swift
@@ -19,16 +19,23 @@ func pack(length: Int, prefix: String, conditions: Array<Condition>.SubSequence,
     // Recursion shouldn't change the total length of the possible pending output and remaining conditions
     assert(prefix.count + conditions.count == length)
 
-    print("\(prefix) -- \((conditions.map { String($0.rawValue) }).joined()) \(damagedLengths)")
+    //print("\(prefix) -- \((conditions.map { String($0.rawValue) }).joined()) \(damagedLengths)")
 
     if conditions.isEmpty {
         if damagedLengths.isEmpty {
-            print("\(indent(prefix.count * 2 + 1))at end -> 1")
+            //print("\(indent(prefix.count * 2 + 1))at end -> 1")
             return [prefix]
         } else {
-            print("\(indent(prefix.count * 2 + 1))expecting more damage -> 0")
+            //print("\(indent(prefix.count * 2 + 1))expecting more damage -> 0")
             return []
         }
+    }
+
+    let totalDamageLeft = damagedLengths.reduce(0, { $0 + $1 })
+    let possibleDamageLeft = conditions.reduce(0, { $0 + ($1 == .operational ? 0 : 1) })
+    // maybe count the guaranteed damage left and use it as a lower bound to continue
+    if totalDamageLeft > possibleDamageLeft {
+        return []
     }
 
     let current = conditions.first!
@@ -37,26 +44,46 @@ func pack(length: Int, prefix: String, conditions: Array<Condition>.SubSequence,
         // Can't pack any damaged length into an operational spot
         return pack(length: length, prefix: prefix + String(current.rawValue), conditions: conditions.dropFirst(), damagedLengths: damagedLengths)
     case .damaged:
-        // If this is guaranteed damaged, need the first damage range to consume it
+        // If we have a definitely (or provisionally assumed) damaged, then we need to have a string of damage matching the given length here and then a separator or the end of the conditions
         guard let firstLength = damagedLengths.first else {
-            print("\(indent(prefix.count * 2 + 1))Have damage in the conditions, but no lengths left -> 0")
+            //print("\(indent(prefix.count * 2 + 1))Have damage in the conditions, but no lengths left -> 0")
             return []
         }
-        if firstLength == 1 {
-            // Entirely consumed. Need the next condition to able to be mapped to operational otherwise it'd be merged
-            let remaining = conditions.dropFirst()
-            guard let separator = remaining.first else {
-                // No trailing separator, but the end of the conditions is an implicit separator
-                return [prefix + String(Condition.damaged.rawValue)]
-            }
-            guard separator != .damaged else {
-                print("\(indent(prefix.count * 2 + 1))XXX damage length of 1, but next is \(separator) -> 0")
+
+        var updatedPrefix = prefix + String(Condition.damaged.rawValue)
+        var remainingConditions = conditions.dropFirst() // first one is the one we just hit
+        var remainingLength = firstLength - 1
+
+        // Consume any remaining required damage length
+        while remainingLength > 0 {
+            guard let next = remainingConditions.first else {
+                //print("\(indent(prefix.count * 2 + 1))XXX remaining damage length of \(remainingLength), but ran out of conditions")
                 return []
             }
-            return pack(length: length, prefix: prefix + String(Condition.damaged.rawValue) + String(Condition.operational.rawValue), conditions: remaining.dropFirst(), damagedLengths: damagedLengths.dropFirst())
+            guard next != .operational else {
+                //print("\(indent(prefix.count * 2 + 1))XXX remaining damage length of \(remainingLength), but next is \(next)")
+                return []
+            }
+
+            updatedPrefix = updatedPrefix + String(Condition.damaged.rawValue)
+            remainingConditions = remainingConditions.dropFirst()
+            remainingLength -= 1
+        }
+
+        // Now, need a separator or the end of conditions
+        if let separator = remainingConditions.first {
+            if separator != .damaged {
+                updatedPrefix = updatedPrefix + String(Condition.operational.rawValue)
+                remainingConditions = remainingConditions.dropFirst()
+
+                return pack(length: length, prefix: updatedPrefix, conditions: remainingConditions, damagedLengths: damagedLengths.dropFirst())
+            } else {
+                //print("\(indent(prefix.count * 2 + 1))XXX need a separator after damage but next is \(separator) -> 0")
+                return []
+            }
         } else {
-            // Used up one unit of the first damaged length
-            return pack(length: length, prefix: prefix + String(Condition.damaged.rawValue), conditions: conditions.dropFirst(), damagedLengths: [firstLength - 1] + damagedLengths.dropFirst())
+            // No trailing separator, but the end of the conditions is an implicit separator. Recurse to handle the case there are more damaged lengths
+            return pack(length: length, prefix: updatedPrefix, conditions: remainingConditions, damagedLengths: damagedLengths.dropFirst())
         }
     case .unknown:
         // Can either assume this spot is damaged or not
@@ -68,20 +95,44 @@ func pack(length: Int, prefix: String, conditions: Array<Condition>.SubSequence,
 
 }
 
-var result = 0
+do {
+    var result = 0
 
-for line in lines {
-    let components = line.split(separator: " ")
-    let conditions = components[0].map { Condition(rawValue: $0)! }
-    let damagedLengths = String(components[1]).numbers(separatedBy: CharacterSet(charactersIn: ","))
+    for line in lines {
+        let components = line.split(separator: " ")
+        let conditions = components[0].map { Condition(rawValue: $0)! }
+        let damagedLengths = String(components[1]).numbers(separatedBy: CharacterSet(charactersIn: ","))
 
-    let options = pack(length: conditions.count, prefix: "", conditions: conditions[...], damagedLengths: damagedLengths[...])
-    print("~~~~ \(line) \(options.count):")
-    options.forEach { print($0) }
-    print("~~~~")
+        let options = pack(length: conditions.count, prefix: "", conditions: conditions[...], damagedLengths: damagedLengths[...])
+        print("~~~~ \(line) \(options.count):")
+        //    options.forEach { print($0) }
+        //    print("~~~~")
 
-    result += options.count
+        result += options.count
+    }
+
+    print("\(result)")
+    //assert(result == 7705) // part 1
 }
 
-print("\(result)")
-// 114033 too high with case that failed on test input
+do {
+    var result = 0
+
+    for line in lines {
+        let components = line.split(separator: " ")
+        let conditions = components[0].map { Condition(rawValue: $0)! }
+        let damagedLengths = String(components[1]).numbers(separatedBy: CharacterSet(charactersIn: ","))
+
+        let repeatedConditions = conditions + [.unknown] + conditions + [.unknown] + conditions + [.unknown] + conditions + [.unknown] + conditions
+        let repeatedLengths = damagedLengths + damagedLengths + damagedLengths + damagedLengths + damagedLengths
+
+        let options = pack(length: repeatedConditions.count, prefix: "", conditions: repeatedConditions[...], damagedLengths: repeatedLengths[...])
+        print("~~~~ \(line) \(options.count):")
+        //    options.forEach { print($0) }
+        //    print("~~~~")
+
+        result += options.count
+    }
+
+    print("\(result)")
+}
