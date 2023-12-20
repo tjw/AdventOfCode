fad818dfaabc97e5253539fcec778b7c7838e3d6
Timothy J. Wood <tjw@omnigroup.com>
2023-12-19 18:10:52 -0800

    Day 17 WIP, still

--- 2023/day17/main.swift
+++ 2023/day17/main.swift
@@ -11,25 +11,73 @@ let lines = Array(Input.lines())
 let map = GridMap<Int>(lines: lines) { _, ch in Int(String(ch))! }
 
 struct Traversal : Comparable {
-    let cost: Int
 
     struct PathElement : Equatable {
         let location: Location2D
         let direction: Location2D // The direction this was entered from, or zero if this is the start
     }
 
+    init(start: Location2D) {
+        self.cost = 0
+        self.path = [Traversal.PathElement(location: start, direction: .zero)]
+    }
+
+    private init(cost: Int, path: [PathElement]) {
+        self.cost = cost
+        self.path = path
+    }
+
+    let cost: Int
     let path: [PathElement]
 
     var location: Location2D {
         path.last!.location
     }
 
-    func pathHasMaximumLengthInDirection(_ dir: Location2D) -> Bool {
-        let pathCount = path.count
-        guard pathCount >= 3 else { return false }
+    func moveInDirection(_ dir: Location2D, map: GridMap<Int>) -> Traversal? {
+        let location = self.location
+        let next = location + dir
 
-        // If we already have three steps in the same direction, we can't continue in that direction
-        return path[pathCount - 1].direction == dir && path[pathCount - 2].direction == dir && path[pathCount - 3].direction == dir
+        guard let nextCost = map[next] else {
+            return nil
+        }
+
+        let candidate = Traversal(cost: cost + nextCost, path: path + [PathElement(location: next, direction: dir)])
+        if candidate.straightLinePathLength > 3 {
+            return nil
+        }
+
+        return candidate
+    }
+
+//    func pathHasMaximumLengthInDirection(_ dir: Location2D) -> Bool {
+//        let pathCount = path.count
+//        guard pathCount >= 3 else { return false }
+//
+//        // If we already have three steps in the same direction, we can't continue in that direction
+//        return path[pathCount - 1].direction == dir && path[pathCount - 2].direction == dir && path[pathCount - 3].direction == dir
+//    }
+
+    var straightLinePathLength: Int {
+        let pathCount = path.count
+        assert(pathCount >= 1) // init(start:) adds at least one element
+
+        if pathCount < 2 {
+            return pathCount - 1
+        }
+
+        var length = 1
+        let dir = path.last!.direction
+
+        for element in path.dropLast().reversed() {
+            if element.direction == dir {
+                length += 1
+            } else {
+                return length
+            }
+        }
+
+        fatalError() // The first shoud have direction (0,0) and no others should
     }
 
     static func < (lhs: Traversal, rhs: Traversal) -> Bool {
@@ -39,7 +87,7 @@ struct Traversal : Comparable {
 
 
 func traverse(map: GridMap<Int>) -> Traversal {
-    let start = Traversal(cost: 0, path: [Traversal.PathElement(location: .zero, direction: .zero)])
+    let start = Traversal(start: .zero)
     let end = Location2D(x: map.width - 1, y: map.height - 1)
 
     var heap = Heap<Traversal>()
@@ -49,38 +97,41 @@ func traverse(map: GridMap<Int>) -> Traversal {
     bestTraversalForLocation[start.location] = start
 
     while true {
-        let best = heap.popMin()!
+        guard let best = heap.popMin() else {
+            return bestTraversalForLocation[end]!
+        }
         print("best at \(best.location), cost \(best.cost), with \(heap.count) remaining")
 
         for dir in Location2D.cardinalDirections {
             guard dir != -best.path.last!.direction else {
                 // Can't turn around
+                print("  skip dir \(dir), which is backwards")
                 continue
             }
 
-            let next = best.location + dir
-            guard let cost = map[next] else { continue }
-
-            if best.pathHasMaximumLengthInDirection(dir) {
+            guard let nextTraversal = best.moveInDirection(dir, map: map) else {
                 continue
             }
 
-            let nextTraversal = Traversal(cost: best.cost + cost, path: best.path + [Traversal.PathElement(location: next, direction: dir)])
-            if nextTraversal.location == end {
-                return nextTraversal
-            }
+//            if nextTraversal.location == end {
+//                return nextTraversal
+//            }
 
-            if let previousBest = bestTraversalForLocation[next] {
-                if previousBest.cost <= nextTraversal.cost {
-                    continue
+            if let previousBest = bestTraversalForLocation[nextTraversal.location] {
+                if previousBest.cost < nextTraversal.cost {
+                    // Keep the existing traversal for this location
+                    print("  \(nextTraversal.location) alreaady reached with cost \(previousBest.cost) compared to \(nextTraversal.cost)")
                 } else {
-                    print("  better path found")
+                    // In the case of the cost being equal, try this traversal too. It might work out better/worse than the original since they have different directions and thus limits on how far they can continue
+                    bestTraversalForLocation[nextTraversal.location] = nextTraversal
+                    heap.push(nextTraversal)
+                    print("  update best cost for \(nextTraversal.location) to \(nextTraversal.cost)")
                 }
+            } else {
+                bestTraversalForLocation[nextTraversal.location] = nextTraversal
+                heap.push(nextTraversal)
+                print("  insert \(nextTraversal.location), cost \(nextTraversal.cost)")
             }
-
-            bestTraversalForLocation[next] = nextTraversal
-            heap.push(nextTraversal)
-            print("  insert \(nextTraversal.location), cost \(nextTraversal.cost)")
         }
 
 //        print("heap now:")
