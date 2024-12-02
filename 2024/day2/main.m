2900fda465ffda3a3edeab8956e7c51f61891e29
Timothy J. Wood <tjw@omnigroup.com>
2024-12-01 20:03:24 -0800

    Prep day 2
    Switch to Xcode 16.1 and clean up Swift language version setting to be on the project (and set to 6)

---
+++ 2024/day2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  day2
+//
+//  Created by Timothy Wood on 12/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
