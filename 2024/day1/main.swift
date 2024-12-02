39c21c9d3d4fdf3fbeb53a120e4eaf8deebbcf9b
Timothy J. Wood <tjw@omnigroup.com>
2024-12-01 19:43:09 -0800

    Day 1

---
+++ 2024/day1/main.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let lines = Input.lines()
+
+let pairs = lines.map { $0.numbers() }
+let lefts = pairs.map { $0[0] } .sorted()
+let rights = pairs.map { $0[1] } .sorted()
+
+// part 1
+do {
+    let total = zip(lefts, rights).reduce(0, { partial, pair in
+        let dist = abs(pair.0 - pair.1)
+        return partial + dist
+    })
+    print("\(total)")
+}
+
+// part 2
+do {
+    var counts = [Int:Int]()
+    rights.forEach { right in
+        counts[right] = (counts[right] ?? 0) + 1
+    }
+
+    let total = lefts.reduce(0, {
+        $0 + $1 * (counts[$1] ?? 0)
+    })
+    print("\(total)")
+}
