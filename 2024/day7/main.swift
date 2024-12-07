4525f8da49402a76f50de184f38b5144dbb1c4ef
Timothy J. Wood <tjw@omnigroup.com>
2024-12-07 00:20:03 -0800

    2024 day 7 pt 2, not yet correct

--- 2024/day7/main.swift
+++ 2024/day7/main.swift
@@ -42,6 +42,59 @@ do {
         }
 
     }
+//    print("\(result)")
+//    assert(result == 28730327770375)
+}
+
+do {
+    var result = 0
+
+    func pow3ish(_ n: Int) -> Int {
+        var result = 1
+        (0..<n).forEach { _ in
+            result *= 3
+        }
+        return result
+    }
+
+    func concat(_ a: Int, _ b: Int) -> Int {
+        // 123 || 456 -> 123456
+        var mult = 1
+        while mult < b {
+            mult *= 10
+        }
+        return a * mult + b
+    }
+
+    for item in items {
+        // Given an item with say, 5 inputs, there are four slots between the numbers to insert either a + or a *. Assign + to 0 and * to 1
+        let upper = pow3ish(item.inputs.count)
+        print("item \(item.result) \(item.inputs), upper \(upper)")
+        for idx in 0..<upper {
+            var x = idx
+            var total = item.inputs.first!
+            for input in item.inputs.dropFirst() {
+                switch x % 3 {
+                case 0:
+                    total += input
+                case 1:
+                    total *= input
+                case 2:
+                    total = concat(total, input)
+                default:
+                    fatalError()
+                }
+                x /= 3
+            }
+            if total == item.result {
+                result += total
+                print("  match on \(idx)!")
+                break // could be multiple ways to go
+            }
+        }
+
+    }
     print("\(result)")
-    assert(result == 28730327770375)
+    // 297914586446459 is too low
+    // 424261660171557 too low
 }
