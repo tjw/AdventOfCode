019c7aaf60c9cb20ff6644c9f03fb7e9e4b3d092
Timothy J. Wood <tjw@omnigroup.com>
2024-12-19 18:57:43 -0800

    day19 pt 2

--- 2024/day19/main.swift
+++ 2024/day19/main.swift
@@ -11,40 +11,72 @@ let sections = Input.sections()
 let availableTowels = sections[0][0].split(separator: ",").map { String($0.trimmingCharacters(in: .whitespaces)) }
 let patterns = sections[1]
 
-print("availableTowels \(availableTowels)")
+do {
+    var hasSolution = [String:Bool]()
 
-var hasSolution = [String:Bool]()
-
-func checkForSolution(_ string: String) -> Bool {
-    if let value = hasSolution[string] {
-        return value
-    }
-    for towel in availableTowels {
-        if string.hasPrefix(towel) {
-            if string == towel {
-                //print("string \(string) -- true")
-                hasSolution[string] = true
-                return true
-            } else {
-                let remainder = string.trimmingPrefix(towel)
-                if checkForSolution(String(remainder)) {
+    func checkForSolution(_ string: String) -> Bool {
+        if let value = hasSolution[string] {
+            return value
+        }
+        for towel in availableTowels {
+            if string.hasPrefix(towel) {
+                if string == towel {
                     //print("string \(string) -- true")
                     hasSolution[string] = true
                     return true
+                } else {
+                    let remainder = string.trimmingPrefix(towel)
+                    if checkForSolution(String(remainder)) {
+                        //print("string \(string) -- true")
+                        hasSolution[string] = true
+                        return true
+                    }
                 }
             }
         }
+        //print("string \(string) -- false")
+        hasSolution[string] = false
+        return false
     }
-    //print("string \(string) -- false")
-    hasSolution[string] = false
-    return false
+
+    let count = patterns.count {
+        let value = checkForSolution($0)
+        // print("# \($0) -- \(value)")
+        return value
+    }
+    print("\(count)")
+    assert(count == 251)
 }
 
-let count = patterns.count {
-    let value = checkForSolution($0)
-    print("# \($0) -- \(value)")
-    return value
+do {
+    var solutionCount = [String:Int]()
+
+    func countSolutions(_ string: String) -> Int {
+        if let count = solutionCount[string] {
+            return count
+        }
+
+        var count = 0
+
+        for towel in availableTowels {
+            guard string.hasPrefix(towel) else { continue }
+
+            if string == towel {
+                count += 1
+            } else {
+                let remainder = string.trimmingPrefix(towel)
+                count += countSolutions(String(remainder))
+            }
+        }
+
+        //print("memo \(string) -- \(count)")
+        solutionCount[string] = count
+        return count
+    }
+
+    let total = patterns.reduce(0, { $0 + countSolutions($1) })
+    print("\(total)")
+    assert(total == 616957151871345)
 }
-print("\(count)")
-assert(count == 251)
+
 
