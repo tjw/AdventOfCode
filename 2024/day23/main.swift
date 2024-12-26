6b013ad4a9b60d78cf60c20235e1dd971f464e47
Timothy J. Wood <tjw@omnigroup.com>
2024-12-25 20:51:06 -0800

    day 23 pt 2

--- 2024/day23/main.swift
+++ 2024/day23/main.swift
@@ -49,25 +49,52 @@ for line in Input.lines() {
     b.connections.insert(a)
 }
 
-var triples = Set<String>()
+do {
+    var triples = Set<String>()
 
-for computer in computerByName.values {
-    for connection in computer.connections {
-        var shared = computer.connections.intersection(connection.connections)
-        if !shared.isEmpty {
-            for neighbor in shared {
-                if computer.name.hasPrefix("t") || connection.name.hasPrefix("t") || neighbor.name.hasPrefix("t") {
-                    let triple = [computer.name, connection.name, neighbor.name].sorted().joined(separator: ",")
-                    triples.insert(triple)
+    for computer in computerByName.values {
+        for connection in computer.connections {
+            let shared = computer.connections.intersection(connection.connections)
+            if !shared.isEmpty {
+                for neighbor in shared {
+                    if computer.name.hasPrefix("t") || connection.name.hasPrefix("t") || neighbor.name.hasPrefix("t") {
+                        let triple = [computer.name, connection.name, neighbor.name].sorted().joined(separator: ",")
+                        triples.insert(triple)
+                    }
                 }
             }
         }
     }
+
+    print("\(triples.count)")
+    assert(triples.count == 1302)
 }
 
-for triple in Array(triples).sorted() {
-    print("\(triple)")
-}
+do {
+    // Looking at https://en.wikipedia.org/wiki/Bron–Kerbosch_algorithm for maximal complete subgraph or clique, which is NP hard. Not sure if the input set is small enough for this to work.
 
-print("\(triples.count)")
-assert(triples.count == 1302)
+    var cliques = Set<String>()
+
+    func BronKerbosch1(R: Set<Computer>, P: Set<Computer>, X: Set<Computer>) {
+        if P.isEmpty && X.isEmpty {
+            let clique = R.map({ $0.name }).sorted().joined(separator: ",")
+            cliques.insert(clique)
+            return
+        }
+
+        var PN = P
+        var XN = X
+
+        for v in P {
+            BronKerbosch1(R: R.union(Set([v])), P: PN.intersection(v.connections), X: XN.intersection(v.connections))
+            PN.remove(v)
+            XN.insert(v)
+        }
+    }
+
+    BronKerbosch1(R: [], P: Set(computerByName.values), X: [])
+
+    let maximumClique = cliques.sorted(by: { (a:String, b:String) -> Bool in return a.count > b.count }).first!
+    print("\(maximumClique)")
+    assert(maximumClique == "cb,df,fo,ho,kk,nw,ox,pq,rt,sf,tq,wi,xz")
+}
